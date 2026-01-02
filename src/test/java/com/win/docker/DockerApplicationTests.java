1c784f11a779bfc3aaae342b1da4018295d69cb2
asukyi <asukyi@email.com>
2026-01-02 22:08:54 +0900

    build workflow

---
+++ src/test/java/com/win/docker/DockerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.win.docker;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DockerApplicationTests {
+}
